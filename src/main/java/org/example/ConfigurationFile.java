c664106c4b920e7b64923a62b63c59f9c979aef7
krishna <kpchaulagain1999@gmail.com>
2024-05-13 09:35:41 +0545

    started spring_framework

---
+++ src/main/java/org/example/ConfigurationFile.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.example.context")
+public class ConfigurationFile {
+//    @Bean
+//    public Student student3(){
+//        Student student3= new Student();
+//        student3.setName("arjen");
+//        student3.setRollNo(123);
+//        student3.setEmail("arjun@gmail.com");
+//        return  student3;
+//    }
+}
---
+++ src/main/java/org/example/context/Student.java
@@ -0,0 +1,54 @@
+package org.example.context;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+    @Value("krishna")
+    private String name;
+    @Value("kpchaulagain1999@gmail.com")
+    private String email;
+    @Value("1")
+    private int rollNo;
+
+    public String getName() {
+        return name;
+    }
+    public  Student(){}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public Student(String name, String email, int rollNo) {
+        this.name = name;
+        this.email = email;
+        this.rollNo = rollNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", rollNo=" + rollNo +
+                '}';
+    }
+}
